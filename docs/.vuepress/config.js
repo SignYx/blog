d01fbb639d141a0567329ceb02a305cfcf931b81
yxq <1062888640@qq.com>
2019-03-11 17:45:19 +0800

    first build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,33 @@
 module.exports = {
-  title: '草丛迷了鹿',
-  description: '不务正业程序员',
+  title: "草丛迷了鹿",
+  description: "不务正业程序员",
   themeConfig: {
-    nav: [
-      { text: 'Github', link: '/' }
+    editLinks: true,
+    docsDir: "docs",
+    nav: [{ text: "Github", link: "/" }],
+    sidebar: [
+      {
+        title: "运维部署",
+        collapsable: false,
+        children: [
+          {
+            title: "前端部署",
+            collapsable: false,
+            path: "/onm/frontend"
+          }
+        ]
+      },
+      {
+        title: "工具",
+        collapsable: false,
+        children: [
+          {
+            title: "git",
+            collapsable: false,
+            path: "/tools/git"
+          }
+        ]
+      }
     ]
   }
-}
+};
